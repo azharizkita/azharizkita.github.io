07e1817ae06bd0b2e1036a612c61c4d2e2a83c9b
azharizkita <azharizkita@icloud.com>
2020-10-14 18:35:40 +0700

    Initial commit

---
+++ precache-manifest.2df7de95e112b793f61579f17c9cdd85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "995396f218a5eaa96ad8878e3b8b13f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aafc9b323ec0223e891a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d7ae23553424605df9ea",
+    "url": "/static/js/2.8751599a.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.8751599a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aafc9b323ec0223e891a",
+    "url": "/static/js/main.2d796771.chunk.js"
+  },
+  {
+    "revision": "65f00b37e4a4474795ae",
+    "url": "/static/js/runtime-main.f8094649.js"
+  }
+]);
